248b3aae7999a48f386e8fb61ad395f1ce4253f0
ankitasingh211 <iankitasing15@gmail.com>
2024-10-07 14:25:09 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,7 +6,18 @@ class Solution(object):
         """
         matching_brackets = {')': '(', '}': '{', ']': '['}
         stack = []
-        pass
+        for char in s:
+            # If the character is a closing bracket
+            if char in matching_brackets:
+                # Pop the top of the stack if it's non-empty, else use a dummy value '#'
+                top_element = stack.pop() if stack else '#'
+                
+                # If the top element doesn't match the corresponding opening bracket
+                if matching_brackets[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push it onto the stack
+                stack.append(char)
 
 
 
